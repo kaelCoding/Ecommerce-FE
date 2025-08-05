12c945f6f1a869e0f0817250eddb480e2ad4469c
khoi <khoi@192.168.0.117.non-exists.ptr.local>
2025-08-05 21:52:24 +0700

    frontend v4

--- src/models/api.js
+++ src/models/api.js
@@ -1,7 +1,8 @@
 import { token } from "@/stores/auth";
 
 // const API_BASE_URL = "http://localhost:8080";
-const API_BASE_URL = "https://ecommerce-be-zixc.onrender.com";
+// const API_BASE_URL = "https://ecommerce-be-zixc.onrender.com";
+const API_BASE_URL = "https://ecommerce-be-production-856c.up.railway.app";
 
 export const api = async (method, url, data) => {
     const urlApi = API_BASE_URL + url;
