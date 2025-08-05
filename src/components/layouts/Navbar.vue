6e2c586712d6d97b73e5d39766da203857b68a4d
khoi <khoi@192.168.0.117.non-exists.ptr.local>
2025-08-06 00:11:06 +0700

    add link admin

--- src/components/layouts/Navbar.vue
+++ src/components/layouts/Navbar.vue
@@ -29,6 +29,14 @@ onBeforeUnmount(() => {
         <a href="#about-us">Về chúng tôi</a>
         <a href="#contact">Liên hệ</a>
       </nav>
+      <div class="nav-actions">
+        <RouterLink to="/admin" class="action-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M16 18a4 4 0 0 0-8 0a4 4 0 0 0 8 0m-2-8a6 6 0 1 0-12 0a6 6 0 0 0 12 0M2 22s2-4 6-4s4 2 6 2s4-2 6-2" />
+          </svg>
+        </RouterLink>
+      </div>
       <!-- <div class="nav-actions">
         <input type="text" class="search-bar" placeholder="Tìm kiếm sản phẩm...">
         <RouterLink to="/admin" class="action-icon">
