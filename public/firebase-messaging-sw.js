64746a3d701ee3eb5d0972ee768a5eb8f6cf87fd
khoi <khoi@192.168.0.101.non-exists.ptr.local>
2025-08-21 21:23:52 +0700

    fix firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,15 @@
 importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js");
-import { firebaseConfig } from "@/main";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCd6VV5jVJXQ_xZbse7yvL3czd91DglmOU",
+  authDomain: "ecommercetunitoku.firebaseapp.com",
+  projectId: "ecommercetunitoku",
+  storageBucket: "ecommercetunitoku.firebasestorage.app",
+  messagingSenderId: "811418653806",
+  appId: "1:811418653806:web:648e869345a18f8e27427f",
+  measurementId: "G-JFNQ2PNPCE"
+};
 
 const app = firebase.initializeApp(firebaseConfig);
 
